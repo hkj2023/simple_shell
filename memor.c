2729af2a12ce53527c02d3e5dcbaa544fb831ede
desu6 <mail@y.cm>
2023-04-19 05:58:04 -0700

    memor.c file is created

---
+++ memor.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * bfree - frees & NULL a pointer
+ * @ptr: address of the pointer
+ * Return: Always 0 (Success)
+ */
+
+int bfree(void **ptr)
+{
+if (ptr && *ptr)
+{
+free(*ptr);
+*ptr = NULL;
+return (1);
+}
+return (0);
+}
